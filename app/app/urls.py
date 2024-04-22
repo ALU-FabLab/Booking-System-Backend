a895c7db5e83297eeff30ba4f38dfdedaaf8e753
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-04-22 17:32:56 +0200

    Modified: Updated project url mappings

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,8 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),
+    path(
+        'api/docs/schema/',
+        SpectacularAPIView.as_view(),
+        name='api-schema'
+    ),
+    path(
+        'api/docs/sche',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs'
+    ),
+    path(
+        'api/docs/',
+        SpectacularRedocView.as_view(url_name='api-schema'),
+        name='api-docs'
+    )
 ]

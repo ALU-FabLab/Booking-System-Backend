2f07b4c65e6c997bfbcc9feae20e16583731b420
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-04-25 16:56:25 +0200

    Preparing for staging

--- restart.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Retart the Docker containers and runs the application
-docker-compose down
-docker-compose up
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker-compose run --rm app sh -c "python manage.py test"

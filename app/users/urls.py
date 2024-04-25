c7739f05a099e92b739365cb093ccc380aa0b781
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-04-25 14:25:31 +0200

    Modified: Added url mappings for user authentication and email verification.

--- app/users/urls.py
+++ app/users/urls.py
@@ -4,10 +4,28 @@ URL mapping for users App APIs.
 from django.urls import path
 from users import views
 
+
+# ! FOR J.W.T Authentication
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 app_name = 'user'
 
 urlpatterns = [
     path('', views.ListCreateUserView.as_view(), name='list-create'),
     path('<int:pk>/', views.DetailUserView.as_view(), name='user'),
-    path('me/', views.ManageProfileView.as_view(), name='me'),
+    path('profile/', views.ManageProfileView.as_view(), name='profile'),
+    path(
+        'activate/<uidb64>/<token>/',
+        views.ActivateAccountAPIView.as_view(),
+        name='activate'
+    ),
+
+    # ! FOR J.W.T Authentication
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
